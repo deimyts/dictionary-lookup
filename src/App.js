12cf17557025fea496686678d690a654bce8832b
Cameron Jones <deimyts@gmail.com>
2019-06-11 09:25:56 -0500

    test: setting selection as state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dictionary</h1>
-      <p>
-        A dictionary, sometimes known as a wordbook, is a collection of words in one or more specific languages, often arranged alphabetically (or by radical and stroke for ideographic languages), which may include information on definitions, usage, etymologies, pronunciations, translation, etc. or a book of words in one language with their equivalents in another, sometimes known as a lexicon. It is a lexicographical reference that shows inter-relationships among the data.
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Dictionary</h1>
+        <p>
+          A dictionary, sometimes known as a wordbook, is a collection of words in one or more specific languages, often arranged alphabetically (or by radical and stroke for ideographic languages), which may include information on definitions, usage, etymologies, pronunciations, translation, etc. or a book of words in one language with their equivalents in another, sometimes known as a lexicon. It is a lexicographical reference that shows inter-relationships among the data.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +12,24 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe.skip('when a word is clicked', () => {
+  it('highlights the word', () => {
+    const app = Enzyme.shallow(<App />);
+    const text = app.find('p');
+    text.simulate('click')
+  })
+})
+
+describe('when a word has been selected', () => {
+  it('passes the start & end indices to the "SelectableText" component', () => {
+    const app = Enzyme.shallow(<App />);
+    const selection = {
+      start: 0,
+      end: 3
+    }
+
+    app.setState({ selection })
+    expect(app.exists('SelectableText')).toBe(true);
+  });
+})
