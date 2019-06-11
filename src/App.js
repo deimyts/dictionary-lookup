0c839a9ab2c09e99c7b274c72bf9af64fbd9cfd1
Cameron Jones <deimyts@gmail.com>
2019-06-11 09:26:38 -0500

    wip: include selectableText component in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import SelectableText from './SelectableText'
 
 class App extends React.Component {
   render() {
@@ -7,7 +8,9 @@ class App extends React.Component {
       <div className="App">
         <h1>Dictionary</h1>
         <p>
-          A dictionary, sometimes known as a wordbook, is a collection of words in one or more specific languages, often arranged alphabetically (or by radical and stroke for ideographic languages), which may include information on definitions, usage, etymologies, pronunciations, translation, etc. or a book of words in one language with their equivalents in another, sometimes known as a lexicon. It is a lexicographical reference that shows inter-relationships among the data.
+          <SelectableText>
+            A dictionary, sometimes known as a wordbook, is a collection of words in one or more specific languages, often arranged alphabetically (or by radical and stroke for ideographic languages), which may include information on definitions, usage, etymologies, pronunciations, translation, etc. or a book of words in one language with their equivalents in another, sometimes known as a lexicon. It is a lexicographical reference that shows inter-relationships among the data.
+          </SelectableText>
         </p>
       </div>
     );
