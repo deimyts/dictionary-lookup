81b8d1e744d73b7613af6d228b77d6b8ba21c0aa
Cameron Jones <deimyts@gmail.com>
2019-06-11 13:55:36 -0500

    refactor: delete dead code

--- src/getWord.js
+++ src/getWord.js
@@ -1,29 +1,5 @@
 const isSpace = (char) => char === ' ';
 
-const getChars = (sourceText, index, result = '', fn) => {
-  const char = sourceText[index]
-  const indexOutOfRange = index >= sourceText.length || index < 0
-  if(indexOutOfRange || isSpace(char)) {
-    return result
-  } else {
-    return fn();
-  }
-}
-
-const getNextChars = (sourceText, index, result = '') => {
-  return getChars(sourceText, index, result, () => {
-    result += sourceText[index]
-    return getNextChars(sourceText, index + 1, result);
-  })
-}
-
-const getPrevChars = (sourceText, index, result = '') => {
-  return getChars(sourceText, index, result, () => {
-    result = sourceText[index] + result;
-    return getPrevChars(sourceText, index - 1, result);
-  })
-}
-
 const getStartIndex = (sourceText, index) => {
   if(isSpace(sourceText[index])) return index + 1;
   if(index <= 0) return 0;
@@ -47,15 +23,8 @@ export default function getWord(sourceText, index) {
     selectionEnd: sourceText.length
   }
 
-  const selectionStart = null
-  const selectionEnd = null
-
-  // let result = sourceText[index];
-
   return {
     selectionStart: getStartIndex(sourceText, index),
     selectionEnd: getEndIndex(sourceText, index + 1)
   }
-  // result = getPrevChars(sourceText, index - 1) + result + getNextChars(sourceText, index + 1)
-  // return result
 }
