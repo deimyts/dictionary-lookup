afe786239c44ef2124a177de46d39983be629df9
Cameron Jones <deimyts@gmail.com>
2019-06-10 21:09:56 -0500

    refactor

--- src/getWord.js
+++ src/getWord.js
@@ -1,6 +1,6 @@
 const isSpace = (char) => char === ' ';
 
-const getNextChars = (sourceText, index, result) => {
+const getNextChars = (sourceText, index, result = '') => {
   if(index < sourceText.length) {
     if(isSpace(sourceText[index])) return result;
     result += sourceText[index]
@@ -10,7 +10,7 @@ const getNextChars = (sourceText, index, result) => {
   }
 }
 
-const getPrevChars = (sourceText, index, result) => {
+const getPrevChars = (sourceText, index, result = '') => {
   if(index >= 0) {
     if(isSpace(sourceText[index])) return result;
     result = sourceText[index] + result;
@@ -28,6 +28,6 @@ export default function getWord(sourceText, index) {
 
   let result = sourceText[index];
 
-  result = getPrevChars(sourceText, index - 1, '') + result + getNextChars(sourceText, index + 1, '');
+  result = getPrevChars(sourceText, index - 1) + result + getNextChars(sourceText, index + 1)
   return result
 }
