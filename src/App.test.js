c17178a5d36254b08314841af0e3a33f79247dfc
Cameron Jones <deimyts@gmail.com>
2019-06-11 09:35:56 -0500

    test: when clicking a word

--- src/App.test.js
+++ src/App.test.js
@@ -13,14 +13,6 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-describe.skip('when a word is clicked', () => {
-  it('highlights the word', () => {
-    const app = Enzyme.shallow(<App />);
-    const text = app.find('p');
-    text.simulate('click')
-  })
-})
-
 describe('when a word has been selected', () => {
   it('passes the start & end indices to the "SelectableText" component', () => {
     const app = Enzyme.shallow(<App />);
@@ -35,3 +27,14 @@ describe('when a word has been selected', () => {
     expect(app.find('SelectableText').prop('selectionEnd')).toBe(1)
   });
 })
+
+describe('when a word is clicked', () => {
+  it('adds the selection to the state', () => {
+    const app = Enzyme.shallow(<App />);
+    const text = app.find('SelectableText');
+    text.simulate('click')
+    expect(app.state('selectionStart')).toBe(0);
+    expect(app.state('selectionEnd')).toBe(5);
+  })
+})
+
