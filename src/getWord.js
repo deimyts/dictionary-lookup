0a3e2ca507560910210c07b0581683d9810b2516
Cameron Jones <deimyts@gmail.com>
2019-06-10 20:36:45 -0500

    refactor

--- src/getWord.js
+++ src/getWord.js
@@ -1,7 +1,8 @@
 export default function getWord(sourceText, index) {
   const invalidArgs = !sourceText || typeof index !== 'number';
+  const indexOutOfRange = index + 1 > sourceText.length || index < 0;
   if(invalidArgs) return '';
-  if(index + 1 > sourceText.length || index < 0) return '';
+  if(indexOutOfRange) return '';
   let result = sourceText[index];
   if(index + 1 < sourceText.length) result = result + sourceText[index+1]
   return result
