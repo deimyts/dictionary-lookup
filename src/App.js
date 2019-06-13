d9774d00c418fa312430789532350b3a04e56ba6
Cameron Jones <deimyts@gmail.com>
2019-06-12 23:44:23 -0500

    refactor: rearrange html elements

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Dictionary</h1>
-        <p>
-          <SelectableText>
+        <SelectableText>
+          <h1>Dictionary</h1>
+          <p>
             A dictionary, sometimes known as a wordbook, is a collection of words in one or more specific languages, often arranged alphabetically (or by radical and stroke for ideographic languages), which may include information on definitions, usage, etymologies, pronunciations, translation, etc. or a book of words in one language with their equivalents in another, sometimes known as a lexicon. It is a lexicographical reference that shows inter-relationships among the data.
-          </SelectableText>
-        </p>
+          </p>
+        </SelectableText>
       </div>
     );
   }
