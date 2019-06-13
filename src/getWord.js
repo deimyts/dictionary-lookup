3fbaa584e3dc8980e2070535b0af44f21a7c20ed
Cameron Jones <deimyts@gmail.com>
2019-06-12 23:07:29 -0500

    refactor: remove comments

--- src/getWord.js
+++ src/getWord.js
@@ -4,7 +4,6 @@ const isSpace = (char) => !validChars.test(char);
 const getStartIndex = (sourceText, index) => {
   if(index < 0) return 0;
   const char = sourceText[index]
-  // console.log('CHAR: ', char, isSpace(char))
   const isInvalid = !char || char.match(validChars) === null;
   if(isInvalid) return index + 1;
   else if(index === 0) return 0;
@@ -13,9 +12,6 @@ const getStartIndex = (sourceText, index) => {
 
 const getEndIndex = (sourceText, index) => {
   const char = sourceText[index]
-  // console.log(`SOURCE: "${sourceText}"`)
-  // console.log(`INDEX: "${index}"`)
-  // console.log('CHAR: ', `"${char}"`, 'INDEX: ', index, 'INVALID?: ', isSpace(char))
   const isInvalid = !char || char.match(validChars) === null;
   if(isInvalid) return index;
   else if(index === sourceText.length - 1) {
@@ -24,12 +20,10 @@ const getEndIndex = (sourceText, index) => {
   }
   
   else return getEndIndex(sourceText, index + 1);
-  // else return getEndIndex(sourceText, index + 1);
 }
 
 
 export default function getWord(sourceText, index) {
-  // console.log('SOURCE: ', `"${sourceText}"`)
   let selectionStart = 0;
   let selectionEnd = 0;
 
@@ -46,12 +40,10 @@ export default function getWord(sourceText, index) {
 
   const char = sourceText[index]
   const isInvalid = !char || char.match(validChars) === null;
-  // console.log('RECURSING')
   if(!isInvalid) { 
     selectionStart = getStartIndex(sourceText, index - 1),
       selectionEnd = getEndIndex(sourceText, index)
   }
-  // selectionEnd: index === sourceText.length - 1 ? sourceText.length : getEndIndex(sourceText, index + 1)
 
   return { selectionStart, selectionEnd }
 }
