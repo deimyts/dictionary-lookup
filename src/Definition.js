ec26cf886cc3c3115aac5e4c9ab05d8b6375b3d0
Cameron Jones <deimyts@gmail.com>
2019-06-13 01:49:50 -0500

    feat: hide text before definition loads

--- src/Definition.js
+++ src/Definition.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Definition.scss';
 
 export default function Definition(props) {
+  const text = props.definition ? `${props.word}: ${props.definition}` : ''
   return (
     <React.Fragment>
-      <div className='definition' style={props.style}>{`${props.word}: ${props.definition}`}</div>
+      <div className='definition' style={props.style}>{text}</div>
       <Overlay handleClick={props.overlayClickHandler} />
     </React.Fragment>
   )
