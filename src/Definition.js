a017d914828ab5063a7506cd8752421908e6688b
Cameron Jones <deimyts@gmail.com>
2019-06-13 01:54:55 -0500

    smoother transitions when displaying popover

--- src/Definition.js
+++ src/Definition.js
@@ -3,9 +3,10 @@ import './Definition.scss';
 
 export default function Definition(props) {
   const text = props.definition ? `${props.word}: ${props.definition}` : ''
+  const status = props.definition ? 'active' : 'inactive'
   return (
     <React.Fragment>
-      <div className='definition' style={props.style}>{text}</div>
+      <div className={`definition ${status}`} style={props.style}>{text}</div>
       <Overlay handleClick={props.overlayClickHandler} />
     </React.Fragment>
   )
