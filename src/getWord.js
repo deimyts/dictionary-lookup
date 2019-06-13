d1ca6144cf601f1b37486a91fd98fcba0e73d70c
Cameron Jones <deimyts@gmail.com>
2019-06-12 21:52:14 -0500

    wip

--- src/getWord.js
+++ src/getWord.js
@@ -1,5 +1,5 @@
-const invalidChars = /[^a-zA-Z]/g
-const isSpace = (char) => invalidChars.test(char);
+const validChars = /[a-zA-Z]/g
+const isSpace = (char) => !validChars.test(char);
 
 const getStartIndex = (sourceText, index) => {
   if(index < 0) return 0;
@@ -11,14 +11,14 @@ const getStartIndex = (sourceText, index) => {
 
 const getEndIndex = (sourceText, index) => {
   const char = sourceText[index]
-  console.log('CHAR: ', `"${char}"`, 'INDEX: ', index, 'INVALID?: ', isSpace(char))
+  // console.log('CHAR: ', `"${char}"`, 'INDEX: ', index, 'INVALID?: ', isSpace(char))
   // if(index > sourceText.length) return sourceText.length;
   // if(index === sourceText.length) return 'baz';
   if(index > sourceText.length) return sourceText.length;
   else if(index === sourceText.length - 1) {
     // if(!isSpace(char)) return 'foobar'
     if(isSpace(char)) return index
-    // return 'bar'
+    return `blah: ${sourceText.length - 1}`
     // console.log('BAR')
     return sourceText.length - 1
   }
@@ -32,18 +32,39 @@ export default function getWord(sourceText, index) {
   const invalidArgs = !sourceText || typeof index !== 'number';
   if(invalidArgs) return {};
   const indexOutOfRange = index < 0 || index >= sourceText.length;
-  if(indexOutOfRange) return {};
-  if(isSpace(sourceText[index])) {
+
+  const char = sourceText[index];
+  if(!char) {
     return {
       selectionStart: 0,
-      selectionEnd: 0
+      selectionEnd: 'foo'
     }
   }
-  if(!invalidChars.test(sourceText)) return {
+
+  console.log('1')
+  if(indexOutOfRange) {
+    return {
+      selectionStart: 0,
+      selectionEnd: 'bar'
+    }
+  }
+  console.log('2')
+  const isInvalid = isSpace(char);
+  console.log(`"${char}" IS INVALID?: `, `"${isInvalid}"`)
+  if(isInvalid) {
+    console.log('IT WAS')
+    return {
+      selectionStart: 0,
+      selectionEnd: 'baz'
+    }
+  }
+  console.log('3')
+  if(validChars.test(sourceText)) return {
     selectionStart: 0,
-    selectionEnd: sourceText.length
+    selectionEnd: 'qux'
   }
 
+  console.log('RECURSING')
   return {
     selectionStart: getStartIndex(sourceText, index - 1),
     selectionEnd: getEndIndex(sourceText, index)
--- src/getWord.test.js
+++ src/getWord.test.js
@@ -30,14 +30,9 @@ describe('getWord()', () => {
     })
   })
 
-  describe('when the source text contains spaces', () => {
-    it.only('should stop when it encounters a space', () => {
-      expect(getWord(' ', 0)).toEqual(result(0, 0))
-      expect(getWord(' a', 0)).toEqual(result(0, 0))
-      expect(getWord('a ', 0)).toEqual(result(0, 1))
-      expect(getWord(' a ', 1)).toEqual(result(1, 2))
+  describe.only('when the source text contains spaces', () => {
+    it('should stop when it encounters a space', () => {
       // expect(getWord(' a', 1)).toEqual(result(1, 2))
-      expect(getWord('abc ', 0)).toEqual(result(0, 3))
       // expect(getWord(' abc', 1)).toEqual(result(1, 4))
       // expect(getWord(' abc', 2)).toEqual(result(1, 4))
       // expect(getWord(' abc ', 1)).toEqual(result(1, 4))
@@ -45,6 +40,34 @@ describe('getWord()', () => {
       // expect(getWord('a bc', 2)).toEqual(result(2, 4))
       // expect(getWord('def abc', 6)).toEqual(result(4, 7))
     })
+
+    describe.only('when the starting index is an invalid character', () => {
+      it('should stop when it encounters a space', () => {
+        expect(getWord(' ', 0)).toEqual(result(0, 'baz'))
+        expect(getWord(' a', 0)).toEqual(result(0, 0))
+        expect(getWord('a ', 1)).toEqual(result(0, 0))
+      })
+    })
+
+    describe('when there are spaces before the start index', () => {
+      it('should stop when it encounters a space', () => {
+        expect(getWord(' a', 1)).toEqual(result(1, 2))
+      })
+    })
+
+    describe('when there are spaces after the start index', () => {
+      it('should stop when it encounters a space', () => {
+        expect(getWord('a ', 0)).toEqual(result(0, 1))
+        expect(getWord('abc ', 0)).toEqual(result(0, 3))
+      })
+    })
+
+    describe('when there are spaces before and after the start index', () => {
+      it('should stop when it encounters a space', () => {
+        expect(getWord(' a ', 1)).toEqual(result(1, 2))
+      })
+    })
+
   })
 
   describe('when the source text contains non-word characters', () => {
