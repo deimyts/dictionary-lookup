b41d48d7a4c8c80f2b2552f31fd0151248311f3c
Cameron Jones <deimyts@gmail.com>
2019-06-13 01:47:55 -0500

    refactor: formatting

--- src/Definition.js
+++ src/Definition.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Definition.scss';
 
 export default function Definition(props) {
-  return <React.Fragment>
-    <div className='definition' style={props.style}>{`${props.word}: ${props.definition}`}</div>
-    <Overlay handleClick={props.overlayClickHandler} />
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <div className='definition' style={props.style}>{`${props.word}: ${props.definition}`}</div>
+      <Overlay handleClick={props.overlayClickHandler} />
+    </React.Fragment>
+  )
 }
 
 export function Overlay(props) {
